e1e1ddf557f83a2533a8aaf539d31610b8f397c9
Yash  Jain <165804028+YashJaingit0074@users.noreply.github.com>
2025-11-13 12:14:13 +0530

    Update footer to credit new contributors

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ const App = () => {
           Ayurveda is a traditional Indian system of medicine that focuses on balance within the body, mind, and spirit. It utilizes natural remedies, lifestyle changes, and dietary practices based on the individual's dosha type: Vata, Pitta, or Kapha.
         </p>
         
-        <p className="made-by">Made by <b>Ayush Parwal</b> with lots of ❤️.</p>
+        <p className="made-by">Made by <b>Yash Jain , Naman Sharma, Ayush Mishra </b> with lots of ❤️.</p>
       </footer>
     </div>
   );
